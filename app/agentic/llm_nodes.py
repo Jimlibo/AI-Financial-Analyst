220061d3fb16a3053629c75f034eb98968cbf14a
Jimlibo <jimlibo13@gmail.com>
2025-02-07 14:36:03 +0200

    add llm node implementation

---
+++ app/agentic/llm_nodes.py
@@ -0,0 +1,31 @@
+"""
+File containing assistant class implementations for the agent nodes of the graph
+"""
+from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_core.messages import AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+from app.agentic.states import GraphState
+
+
+class LLMNode:
+    """
+    Class implementation for graph nodes that use LLMs
+    """
+    def __init__(self, runnable: Runnable, prompt: ChatPromptTemplate, name: str | None=None):
+        self.llm = prompt | runnable
+        self.name = "llm_node" if name is None else name
+
+    def __call__(self, state: GraphState, config: RunnableConfig | None=None) -> dict:
+        """
+        Make LLM call based on input state and update the state messages list with the 
+        response.
+        """
+        # get the response from the LLM
+        response = self.llm.invoke(state)
+
+        # handle different types of responses
+        if type(response) == str:
+            return {"messages": [AIMessage(content=response)]}
+        else:
+            return {"messages": response}
