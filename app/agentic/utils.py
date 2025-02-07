1c21db7770c44e46080fdb8a947380234b494d29
Jimlibo <jimlibo13@gmail.com>
2025-02-07 14:22:40 +0200

    add utility functions

---
+++ app/agentic/utils.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def extract_text_from_url(url: str) -> str:
+    """
+    Extracts text from a given url and returns it
+    in a human-readable format.
+    """
+    # get the html content from the url
+    html = urlopen(url).read()
+    # parse the html using bs4
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract() 
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # drop blank lines and form the final extracted text
+    text = '\n'.join(line for line in lines if line)
+    return text
+
+extract_text_from_url("https://www.investopedia.com/us-stocks-kind-of-inflated-jamie-dimon-davos-donald-trump-8778439")
