acc2d817fcd1e114e28336c4570febabf381696b
Jimlibo <jimlibo13@gmail.com>
2025-02-07 15:00:11 +0200

    add template prompts for llm nodes

--- app/agentic/utils/prompts.py
+++ app/agentic/utils/prompts.py
@@ -1,52 +1,34 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-TECHNICAL_ANALYST_PROMPT = ChatPromptTemplate.from_messages(
+TECHNICAL_ANALYSIS_PROMPT = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-             """
-            You are a technical analyst specializing in evaluating company (whose ticker symbol is {stock_ticker}) performance
-            based on stock prices, technical indicators, and financial metrics. Your task is to provide a comprehensive
-            summary of the fundamental analysis for a given stock.
-
-            You have access to the following tools:
-            1. **get_stock_prices**: Retrieves the latest stock price, historical price data and technical
-               Indicators like RSI, MACD, Drawdown and VWAP.
-            2. **get_financial_metrics**: Retrieves key financial metrics, such as revenue, earnings per share (EPS),
-               price-to-earnings ratio (P/E), and debt-to-equity ratio.
-
-            ### Your Task:
-            1. **Input Stock Symbol**: Use the provided stock symbol to query the tools and gather the relevant information.
-            2. **Analyze Data**: Evaluate the results from the tools and identify potential resistance, key trends, strengths,
-            or concerns.
-            3. **Provide Summary**: Write a concise, well-structured summary that highlights:
-                - Recent stock price movements, trends and potential resistance.
-                - Key insights from technical indicators (e.g., whether the stock is overbought or oversold).
-                - Financial health and performance based on financial metrics.
-
-            Be sure to include all indicators and financial metrics along with your summary.
-            Give the above summary to the Risk Manager, so that he can decide the optimal course of action regarding the 
-            specific stock.
-                
-
-            ### Constraints:
-            - Use only the data provided by the tools.
-            - Avoid speculative language; focus on observable data and trends.
-            - If any tool fails to provide data, clearly state that in your summary.
-
-            ### Output Format:
-            Respond in the following format:
-            "stock": "<Stock Symbol>",
-            "price_analysis": "<Detailed analysis of stock price trends>",
-            "technical_indicators: "<all technical indicator in JSON format>"
-            "technical_analysis": "<Detailed time series Analysis from ALL technical indicators>",
-            "financial_metrics": "<all financial metrics in a JSON format>"
-            "financial_analysis": "<Detailed analysis from financial metrics>",
-            "summary": "<A detailed summary of the stock based on the previous findings>"
-
-            Ensure that your response is objective, concise, and actionable.
             """
-        ),
-        ("placeholder", "{messages}")
+
+            """
+        )
+    ]
+)
+
+SENTIMENT_ANALYSIS_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+
+            """
+        )
+    ]
+)
+
+VALUATION_ANALYSIS_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+
+            """
+        )
     ]
 )
