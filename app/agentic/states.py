7bba7ffb7ea20bf486c8f8d948b9dcab051198e4
Jimlibo <jimlibo13@gmail.com>
2025-02-07 14:25:34 +0200

    add new state attributes

--- app/agentic/states.py
+++ app/agentic/states.py
@@ -7,8 +7,15 @@ from langgraph.graph.message import AnyMessage, add_messages
 
 # State class for the workflow
 class AgentState(TypedDict):
-    # list of messages seen by all agents so that they have access to previous outputs
+    # list of messages seen by all LLMs
     messages: Annotated[list[AnyMessage], add_messages]
+
     # stock ticker (i.e. AAPL, JPM, etc.) and exchange marker (i.e., NYSE)
     stock_ticker: str
     stock_exchange: str
+
+    # financial data regarding given stock
+    stock_price_indicators: dict
+    financial_metrics: dict
+    financial_statements: list
+    news_results: list
